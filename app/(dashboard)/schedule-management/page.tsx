1bd129dbb50e14df19f5b80cfea50f5b28fc207f
Nishan Khanal <khanal.nishan28@gmail.com>
2025-10-31 22:17:54 +0545

    feat: init schedule management

---
+++ app/(dashboard)/schedule-management/page.tsx
@@ -0,0 +1,11 @@
+import ScheduleManagementPage from "@/modules/scheduleManagement/scheduleManagementPage"
+
+const page = () => {
+  return (
+    <div>
+      <ScheduleManagementPage />
+    </div>
+  )
+}
+
+export default page
---
+++ modules/scheduleManagement/ScheduleManagementPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ScheduleManagementPage = () => {
+  return (
+    <div>Schedule Management Page</div>
+  )
+}
+
+export default ScheduleManagementPage
